e58d6d85b065750f1e512bb7a3ba1fb23363bb2b
jgranados4 <jgranados@est.ups.edu.ec>
2024-09-17 20:19:27 -0500

    feat: Agregar rutas de productos y componentes relacionados
    Agrega las rutas de productos y componentes relacionados al archivo de rutas de la aplicación.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './shared/ui/header/header.component';
+const UI = [HeaderComponent];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UI],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'store';
